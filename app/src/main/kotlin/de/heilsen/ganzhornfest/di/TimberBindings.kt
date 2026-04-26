c970ffd96304a888763ba07f31a00c1daa5a1cec
Sebastian Heil <heilsen@sebastianheil.de>
2026-04-26 17:23:20 +0200

    feat: replace TimberModule with @ContributesTo TimberBindings

---
+++ app/src/main/kotlin/de/heilsen/ganzhornfest/di/TimberBindings.kt
@@ -0,0 +1,26 @@
+package de.heilsen.ganzhornfest.di
+
+import de.heilsen.ganzhornfest.BuildConfig
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.BindingContainer
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.ElementsIntoSet
+import dev.zacsweers.metro.Multibinds
+import dev.zacsweers.metro.Provides
+import timber.log.Timber
+
+@ContributesTo(AppScope::class)
+interface TimberMultibinding {
+    @Multibinds(allowEmpty = true)
+    fun timberTrees(): Set<@JvmSuppressWildcards Timber.Tree>
+}
+
+@ContributesTo(AppScope::class)
+@BindingContainer
+object TimberBindings {
+    @Provides
+    @ElementsIntoSet
+    fun debugTrees(): Set<@JvmSuppressWildcards Timber.Tree> = buildSet {
+        if (BuildConfig.DEBUG) add(Timber.DebugTree())
+    }
+}
