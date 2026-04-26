b5fb2ede254f2662d772106632b380c25be39260
Sebastian Heil <heilsen@sebastianheil.de>
2026-04-26 17:23:55 +0200

    feat: update GanzhornfestApplication to use Metro createGraphFactory

--- app/src/main/kotlin/de/heilsen/ganzhornfest/GanzhornfestApplication.kt
+++ app/src/main/kotlin/de/heilsen/ganzhornfest/GanzhornfestApplication.kt
@@ -3,9 +3,9 @@ package de.heilsen.ganzhornfest
 import android.app.Application
 import de.heilsen.ganzhornfest.di.AppComponent
 import de.heilsen.ganzhornfest.di.AppComponentProvider
-import de.heilsen.ganzhornfest.di.DaggerAppComponent
-import de.heilsen.ganzhornfest.di.appScope
+import de.heilsen.ganzhornfest.di.appGraph
 import de.heilsen.ganzhornfest.di.getValue
+import dev.zacsweers.metro.createGraphFactory
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -15,14 +15,16 @@ interface EntryPoint {
 
 class GanzhornfestApplication : Application(), AppComponentProvider {
 
-    override val appComponent: AppComponent by lazy { DaggerAppComponent.factory().create(this) }
+    override val appComponent: AppComponent by lazy {
+        createGraphFactory<AppComponent.Factory>().create(this)
+    }
 
     @Inject
     lateinit var timberTrees: Set<@JvmSuppressWildcards Timber.Tree>
 
     override fun onCreate() {
         super.onCreate()
-        val entrypoint: EntryPoint by appScope
+        val entrypoint: EntryPoint by appGraph
         entrypoint.inject(this)
         Timber.plant(*timberTrees.toTypedArray())
     }
